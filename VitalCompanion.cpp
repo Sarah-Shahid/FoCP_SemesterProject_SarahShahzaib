f716a3ba1172b9236e85e36a95e11bf8f08a6e2c
Shah-zaibaly <shahxaib269@gmail.com>
2025-12-20 03:04:46 +0500

    Updated VitalCompanion.cpp

--- VitalCompanion.cpp
+++ VitalCompanion.cpp
@@ -1,95 +1,90 @@
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<ctime>
-#include<cstdlib>
-#include<iomanip>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <limits>
 
 using namespace std;
 
-struct UserProfile {
+// ==================== STRUCTURES ====================
 
+struct UserProfile {
     string name;
     int age;
-    string gender;
-    string bloodgroup;
-    bool profileExists;
-    double weight;
-    double height;
-    long long int phone_number;
-
+    string conditions[5];
+    int conditionCount;
+    string wakeTime;
+    string sleepTime;
 };
 
 struct Medicine {
-
-    int medicineID;
     string name;
-    string dosage; 
-    string frequency;
-
+    string dosage;
+    string time;
+    bool takenToday;
 };
 
-struct Appointments {
-
-    int appointmentID;
+struct HealthReading {
     string date;
-    string time;
-    string doctorName;
-    bool completed;
-
-};
-
-struct HealthReadings {
-
-    string date;
-    string time;
     int systolic;
     int diastolic;
-    int heartrate;
-    int sugarlevel;
-
-
+    int sugar;
+    int heartRate;
 };
 
-struct Mood_Journaling {
-
+struct MoodEntry {
     string date;
-    string time;
     string mood;
-    string journal_notes;
-
+    string note;
+    int score;
 };
 
-struct EmergencyContacts {
-
+struct EmergencyContact {
     string name;
-    long long int phone_number;
+    string phone;
     string relation;
-
 };
 
-struct IslamicSection {
-
-    string verse;
-    string dua;
-    string daily ;
-    
-};
-
-struct Games {
-
-    string game;
-    
-};
-
-struct IncidentLog {
-
+struct Incident {
     string date;
-    string time;
-    string incidentType;
+    string type;
     string description;
-
+    int severity;
 };
+
+// ==================== GLOBAL VARIABLES ====================
+
+UserProfile user;
+Medicine medicines[20];
+int medicineCount = 0;
+HealthReading healthReadings[50];
+int healthCount = 0;
+MoodEntry moods[30];
+int moodCount = 0;
+EmergencyContact contacts[10];
+int contactCount = 0;
+Incident incidents[30];
+int incidentCount = 0;
+
+string islamicVerses[5] = {
+    "Indeed, with hardship comes ease. (Quran 94:6)",
+    "And He is with you wherever you are. (Quran 57:4)",
+    "So remember Me; I will remember you. (Quran 2:152)",
+    "In the remembrance of Allah do hearts find rest. (Quran 13:28)",
+    "Allah does not burden a soul beyond that it can bear. (Quran 2:286)"
+};
+
+string duas[5] = {
+    "Rabbi zidni ilma - My Lord, increase me in knowledge",
+    "Allahumma inni as'aluka al-'afiyah - O Allah, I ask You for well-being",
+    "Rabbi ishrah li sadri - My Lord, expand for me my chest",
+    "Hasbunallahu wa ni'mal wakeel - Allah is sufficient for us",
+    "La hawla wa la quwwata illa billah - No power except with Allah"
+};
+
+const string CAREGIVER_PIN = "1234";
 
 
 int main(){
